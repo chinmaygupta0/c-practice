fd942fc2f945edfe27b90e5fb318e665eb7cce8a
chinmaygupta0 <116647841+chinmaygupta0@users.noreply.github.com>
2023-04-15 00:45:18 +0530

    Add files via upload

---
+++ good turn.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num,num2,sum;
+    
+    cin >> num ;
+   
+    cin >> num2 ;
+    sum=num+num2;
+    if (sum > 6){
+        cout << "yes" ;
+    }
+    
+    else {
+    cout << " no";
+    }
+
+	return 0;
+}
