d682f127d34d49fc0e19fbf022bfe4e3ed9bbc71
chinmaygupta0 <chinmaygupta0987@gmail.com>
2023-04-22 23:02:29 +0530

    code

---
+++ codechef/candivide.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int N;
+	    cin >> N;
+	    if(N%3==0)
+	    {
+	       cout << "yes" << endl;
+	    }
+	else
+	{
+	    cout << "no" << endl;
+	}
+	}
+	return 0;
+}
