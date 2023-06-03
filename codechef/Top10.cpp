ef4a2c85d8eced136d7996bdbf94b060aa296f95
chinmaygupta0 <chinmaygupta0987@gmail.com>
2023-06-03 23:10:12 +0530

    code

---
+++ codechef/Top10.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int t;
+	cin >> t;
+	while(t--)
+{
+    int X;
+    cin>>X;
+    if(X<=10)
+    {
+        cout<<"yes"<<endl;
+    }
+    else{
+        cout<<"no"<<endl;
+    }
+}	
+
+	return 0;
+}
---
+++ codechef/chef plys ludo.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int x;
+	    cin>>x;
+	    if(x==6)
+	    {
+	        cout<<"yes"<<endl;
+	        
+	    }
+	    else{
+	        cout<<"no"<<endl;
+	    }
+	}
+	
+	
+	
+	
+	
+	
+	return 0;
+}
