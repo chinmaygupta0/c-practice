396764b26844f2fb56df2f41d9f71e4edfec747f
chinmaygupta0 <chinmaygupta0987@gmail.com>
2023-06-05 13:43:22 +0530

    code

---
+++ codechef/water consumption.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int t;
+	cin >> t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(x>=2000)
+	    {
+	        cout<<"yes"<<endl;
+	 
+	    }
+	    else {
+	        cout<<"no"<<endl;
+	    }
+	    
+	    
+	}
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
